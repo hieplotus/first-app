11591441ee55045fa265fde4957a85b8ea6f0e39
hieplotus <hieph@lotusoutsourcing.com>
2022-03-15 08:32:53 +0700

    update form

--- src/Exercises/BaiTapForm/DanhSachHocSinh.js
+++ src/Exercises/BaiTapForm/DanhSachHocSinh.js
@@ -6,16 +6,21 @@ export default function DanhSachHocSinh(props) {
   const renderList = () => {
     return list.map((person, index) => {
       return (
-        <div className="row d-flex">
-          <div className="col-6" key={index}>
-            <p>Họ tên:{person.userName}</p>
-            <p>Email:{person.email}</p>
-            <p>Sdt:{person.phone}</p>
+        <div className="row ">
+          <div className="col-6 d-flex justify-content-between " key={index}>
+            <div className="infor-person  border border-primary mb-4 p-3">
+              <p>Họ tên:{person.userName}</p>
+              <p>Email:{person.email}</p>
+              <p>Sdt:{person.phone}</p>
+            </div>
+            <div className="delete-button">
+              <button className="btn btn-danger" onClick={() => { props.xoaPerson(person.userName) }}>X</button>
+            </div>
 
           </div>
-          <div className="col-6">
-            <button onClick={()=>{props.xoaPerson(person.userName)}}>X</button>
-          </div>
+          {/* <div className="col-6">
+     
+          </div> */}
 
         </div>
       )
--- src/Exercises/BaiTapForm/Form.js
+++ src/Exercises/BaiTapForm/Form.js
@@ -31,14 +31,14 @@ export default function Form() {
 
   }
 
-  const xoaPerson = (userName)=> {
+  const xoaPerson = (userName) => {
     let index = danhSach.list.findIndex(ds => ds.userName === userName);
     if (index !== -1) {
       danhSach.list.splice(index, 1);
-  }
+    }
     let danhSachCapNhat = [...danhSach.list];
     setDanhSach({
-      list:danhSachCapNhat
+      list: danhSachCapNhat
     })
   }
 
@@ -47,27 +47,29 @@ export default function Form() {
       <form onSubmit={handleSaveDanhSach}>
         <div className="row">
           <div className="col-12">
-            <div className="group">
-              <input value={info.values.userName} name="userName" required type="text" onChange={handleChangeValue} />
+            <div className="form-group col-6">
+
+              <small id="userName" className="form-text text-muted mb-2">Họ tên</small>
+              <input className="form-control" name="" id="" aria-describedby="userName" placeholder="" value={info.values.userName} name="userName" required type="text" onChange={handleChangeValue} />
               <span className="highlight" />
               <span className="bar" />
-              <label>Họ tên</label>
+
             </div>
           </div>
           <div className="col-12">
-            <div className="group">
-              <input value={info.values.email} name="email" type="email" required onChange={handleChangeValue} />
+            <div className="form-group col-6">
+              <small id="email" className="form-text text-muted mb-2">Email</small>
+              <input className="form-control" name="" id="" aria-describedby="userName" placeholder="" value={info.values.email} name="email" type="email" required onChange={handleChangeValue} />
               <span className="highlight" />
               <span className="bar" />
-              <label>Email</label>
             </div>
           </div>
           <div className="col-12">
-            <div className="group">
-              <input value={info.values.phone} name="phone" type="text" required onChange={handleChangeValue} />
+            <div className="form-group col-6">
+              <small id="phone" className="form-text text-muted mb-2">Số điện thoại</small>
+              <input className="form-control" name="" id="" aria-describedby="phone" placeholder="" value={info.values.phone} name="phone" type="text" required onChange={handleChangeValue} />
               <span className="highlight" />
               <span className="bar" />
-              <label>Số điện thoại</label>
             </div>
           </div>
         </div>
@@ -75,11 +77,17 @@ export default function Form() {
 
 
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <button className="btn text-white bg-dark " style={{ fontSize: 25 }}>Lưu</button>
         </div>
       </form>
-        <DanhSachHocSinh list={danhSach.list} xoaPerson={xoaPerson}/>
+      <DanhSachHocSinh list={danhSach.list} xoaPerson={xoaPerson} />
     </div>
   )
 }
+{/* <div class="form-group">
+  <label for=""></label>
+  <input type="text"
+    class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+  <small id="helpId" class="form-text text-muted">Help text</small>
+</div> */}
